aa3ef6756e24bb221e638720c9579a22d0dba800
Casper da Costa-Luis <casper.dcl@physics.org>
2020-07-19 19:38:42 +0100

    let pyextract use stdin

--- pyextract.py
+++ pyextract.py
@@ -5,13 +5,15 @@ Options:
   -h, --help  : print this message and exit
 
 Arguments:
-  <script>  : path to a `*.py` file
+  <script>  : path to a `*.py` file (use `-` for stdin)
   <function>  : names of functions to print
 """
 import importlib
 import inspect
 import os
-from os import path
+from shutil import rmtree
+import sys
+import tempfile
 
 from argopt import argopt
 
@@ -20,11 +22,27 @@ parser = argopt(__doc__)
 
 def main(argv=None):
     args = parser.parse_args(args=argv)
-    os.environ.setdefault("PYTHONPATH", os.curdir)
-    mod = importlib.import_module(path.splitext(path.basename(args.script))[0])
 
-    for func in args.function:
-        print(inspect.getsource(getattr(mod, func)))
+    tmpdir = None
+    try:
+        if args.script == "-":
+            tmpdir = tempfile.mkdtemp("pyext")
+            args.script = os.path.join(tmpdir, "pyext_module.py")
+            with open(args.script, "w") as fd:
+                fd.write(sys.stdin.read())
+            sys.path.insert(0, tmpdir)
+        else:
+            sys.path.insert(0, os.curdir)
+
+        mod = importlib.import_module(
+            os.path.splitext(os.path.basename(args.script))[0]
+        )
+
+        for func in args.function:
+            print(inspect.getsource(getattr(mod, func)), end="")
+    finally:
+        if tmpdir:
+            rmtree(tmpdir)
 
 
 if __name__ == "__main__":
